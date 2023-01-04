acc927d17ae8cd7231ed93b042cb1168dc8f3e14
Ryan Kelley <ryanlinksin@gmail.com>
2023-01-03 19:24:27 -0800

    "prepare again for digitalocean

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
+DEBUG = str(os.environ.get('DEBUG')) == "0" # 1 == True
 
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
 ALLOWED_HOSTS = []
